30f7815d6d08e747646e7125547793a4ccda2758
BrenLau <blau4000@gmail.com>
2023-10-04 17:46:22 -0400

    got server started

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ const express = require('express');
 const app = express()
 
 app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send("hello world")
+})
+
+
+
+app.listen(3000)
+
+
+
+
+module.exports = app;
